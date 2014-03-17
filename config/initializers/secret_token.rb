83caded1eb6eef5b83c032f5db9c504aabf82ecd
Taylor Smith <twsmith89@gmail.com>
2014-03-17 11:42:57 -0500

    init commit. add ds_store, application.yml, and database.yml to gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yourownstagram::Application.config.secret_key_base = 'de0264e9b7c1f05c02d7124dd454776ea03bcc6e2875613a2e44b50398b6ad3bfffa867185f81d8c100e90841217066a25cf178235dd4aae0e2b4a7f3c113ad1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yourownstagram::Application.config.session_store :cookie_store, key: '_yourownstagram_session'
