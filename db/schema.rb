ceaba19241b72f110ae0259827423759e80bf702
Taylor Smith <twsmith89@gmail.com>
2014-03-17 16:25:21 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317192231) do
+ActiveRecord::Schema.define(version: 20140317212413) do
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
