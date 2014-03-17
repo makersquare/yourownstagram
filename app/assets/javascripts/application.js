4d69f8d32c48de6c78e882e9b7c435895b3622ff
Taylor Smith <twsmith89@gmail.com>
2014-03-17 16:44:20 -0500

    rails g backbone:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/yourownstagram
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
