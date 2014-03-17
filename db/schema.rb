33e167643cdd51d1b5e567593e6ae8b4aaea0b38
Taylor Smith <twsmith89@gmail.com>
2014-03-17 14:34:17 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317185133) do
+ActiveRecord::Schema.define(version: 20140317192231) do
+
+  create_table "images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
